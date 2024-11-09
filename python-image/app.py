c2fc732961240a25a42ce28354f8eecf2ff9f8d9
matthew-l-craig <matt@matthewlcraig.net>
2024-11-09 12:23:46 -0500

    add some stuff

--- python-image/app.py
+++ python-image/app.py
@@ -1,4 +1,9 @@
 import os
+from time import sleep
 print("This is my first image.")
 print(f"Current working directory: {os.getcwd()}")
 print(os.listdir())
+
+name = input("What's your name?")
+
+print(f"Hello, {name}! What a beautiful name! It really suits you.")
